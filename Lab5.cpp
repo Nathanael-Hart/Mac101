d1d69490f674cb3b812871b465b8f382d32197bf
Nathanael-Hart <xnathanaelhartx@gmail.com>
2025-10-21 19:16:58 -0400

    Add files via upload

---
+++ Lab5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int riders;
+	double total = 0;
+	cout << "Enter the number of riders: " << endl;
+	cin >> riders;
+	for (int swipe = 1; swipe <= riders; ++swipe){
+		if (swipe % 3 == 0){
+		total = total + 0;}
+		else if (swipe % 5 == 0){
+		total = total + 1.45;}
+		else {
+		total = total + 2.90;}
+		cout << total << endl;
+	}
+	return 0;
+}
