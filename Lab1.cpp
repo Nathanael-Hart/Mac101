92028a25c242b428244f5ef0b5ca99e7667b9677
Nathanael-Hart <xnathanaelhartx@gmail.com>
2025-12-10 21:54:03 -0500

    Create Lab1.cpp

---
+++ Lab1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "My name is Nathanael." << std::endl;
+    std::cout << "I am a first year student, at Laguardia CC." << std::endl;
+    std::cout << "My favorite movie is THERE WILL BE BLOOD." << std::endl;
+
+    return 0;
+}
