2da30aa5f50ebc8aabfa474f89f843e801164ebe
Nathanael-Hart <xnathanaelhartx@gmail.com>
2025-12-18 14:53:49 -0500

    Update Lab4.cpp

--- Lab4.cpp
+++ Lab4.cpp
@@ -1,71 +1,53 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
+
+int extractNumber(const string& s) {
+    string digits;
+    for (char c : s) {
+        if (isdigit(c))
+            digits += c;
+        else if (!digits.empty())
+            break;
+    }
+    return digits.empty() ? -1 : stoi(digits);
+}
 
 int main() {
     string location;
-
     cout << "Enter your location: ";
     getline(cin, location);
 
-    // Check street names first
     if (location == "Canal St") {
-        cout << "This is Lower Manhattan" << endl;
+        cout << "This is Lower Manhattan\n";
     }
     else if (location == "Chambers St") {
-        cout << "This is Financial District" << endl;
+        cout << "This is Financial District\n";
     }
     else if (location == "Fulton St") {
-        cout << "This is Financial District" << endl;
+        cout << "This is Financial District\n";
     }
     else {
-        // Assume it's a number
-        int num = stoi(location);
+        int num = extractNumber(location);
 
-        if (num > 207) {
-            cout << "Bronx" << endl;
+        if (num == -1) {
+            cout << "Could not determine location\n";
         }
-        else if (num > 190) {
-            cout << "Inwood" << endl;
-        }
-        else if (num > 181) {
-            cout << "Upper Manhattan" << endl;
-        }
-        else if (num > 168) {
-            cout << "Pa'Republica Dominicana" << endl;
-        }
-        else if (num > 145) {
-            cout << "Harlem" << endl;
-        }
-        else if (num > 124) {
-            cout << "Central Harlem" << endl;
-        }
-        else if (num > 59) {
-            cout << "It moved Jerry!" << endl;
-        }
-        else if (num > 50) {
-            cout << "Central Park West" << endl;
-        }
-        else if (num > 42) {
-            cout << "Hell's Kitchen" << endl;
-        }
-        else if (num > 34) {
-            cout << "You want to buy my mixtape man?" << endl;
-        }
-        else if (num > 23) {
-            cout << "Midtown West" << endl;
-        }
-        else if (num > 14) {
-            cout << "Chelsea" << endl;
-        }
-        else if (num > 4) {
-            cout << "Greenwich Village" << endl;
-        }
-        else if (num > 0) {
-            cout << "West Village" << endl;
-        }
-        else {
-            cout << "Staten Island" << endl;
-        }
+        else if (num > 207) cout << "Bronx\n";
+        else if (num > 190) cout << "Inwood\n";
+        else if (num > 181) cout << "Upper Manhattan\n";
+        else if (num > 168) cout << "Pa'Republica Dominicana\n";
+        else if (num > 145) cout << "Harlem\n";
+        else if (num > 124) cout << "Central Harlem\n";
+        else if (num > 59)  cout << "I was in the pool!!\n";
+        else if (num > 50)  cout << "Central Park West\n";
+        else if (num > 42)  cout << "Its flipping RAW!\n";
+        else if (num > 34)  cout << "You want to buy my mixtape man?\n";
+        else if (num > 23)  cout << "Midtown West\n";
+        else if (num > 14)  cout << "Chelsea\n";
+        else if (num > 4)   cout << "Greenwich Village\n";
+        else if (num > 0)   cout << "West Village\n";
+        else                cout << "Staten Island\n";
     }
 
     return 0;
